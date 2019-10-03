b5f345bf665ef54c9f6a4815850005f4e8110020
Tao Long <iamtaolong@gmail.com>
2019-10-03 15:12:50 -0400

    3-file

--- main.cpp
+++ main.cpp
@@ -13,6 +13,21 @@ public:
     void setSalary(double salary);
 /* 1) create a getter and setter for each variable in the class*/
 
+    static void setName(string name){};
+    static void setID(string id){};
+    static void setAddress(string addess){};
+    static void setDoB(string DoB){};
+    static void setDegree(string degree){};
+    string getName(){return name;};
+    string getID(){return id;};
+    string getAdress(){return address;};
+    string getDoB(){return DoB;};
+    string getDegree(){return degree;};
+
+
+
+
+
 
 private:
     double salary;
@@ -27,8 +42,20 @@ void Faculty::setSalary(double s){
 
 int main() {
     Faculty prof;
+    Faculty prof2;
+
 
 /* 3) use the implemented methods to fill prof' info and print them out */
+
+    prof.setName("Tao");
+    prof.setID("12345");
+    prof.setAddress("Magnolia Hall");
+    prof.setDoB("CSC");
+    prof.setDegree("Bachelor");
+    prof.getName();
+    prof.getAdress();
+    prof.getDoB();
+    prof.getDegree();
 
     return 0;
 }
